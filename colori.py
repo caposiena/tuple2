6dde97056521c85c4fe5a99a46ffb284412ab37f
rcaposiena-consulting <r.caposiena@gmail.com>
2025-12-10 14:24:31 +0100

    Create colori.py

---
+++ colori.py
@@ -0,0 +1,13 @@
+# ESERCIZIO: Tupla colori
+
+# Crea tupla con 3 colori
+colori = ("rosso", "blu", "rosso")
+print("Tupla colori:", colori)
+
+# Stampa primo e ultimo
+print("Primo colore:", colori[0])
+print("Ultimo colore:", colori[-1])
+
+# Conta quante volte compare "rosso"
+conteggio = colori.count("rosso")
+print("Il colore 'rosso' compare", conteggio, "volte")
